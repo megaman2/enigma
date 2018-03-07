b506f5ea2c0e33b9addbd773c41bef4be9588604
pbakhtiari <pbakhtiari@ITEM-AX31336.wks.axway.int>
2018-03-07 11:26:25 +0100

    List algos Cipher

---
+++ src/main/java/org/caulfield/enigma/crypto/BCAlgorithmsViewer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.caulfield.enigma.crypto;
+
+import java.security.Provider;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+/**
+ *
+ * @author pbakhtiari
+ */
+public class BCAlgorithmsViewer {
+
+    public static void main(String[] args) {
+        Security.addProvider(new BouncyCastleProvider());
+        Provider p = Security.getProvider("BC");
+        for (Provider.Service s : p.getServices()) {
+            if (s.getType().equals("Cipher")) {
+                System.out.println(s.getAlgorithm());
+                System.out.println("\t" + s.getType() + " " + s.getAlgorithm() + " " + s.toString());
+            }
+        }
+    }
+}
